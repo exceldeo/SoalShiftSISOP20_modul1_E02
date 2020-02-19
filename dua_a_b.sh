179fb7d9a8a85872a4ca90b57ed78094f4a55262
exceldeo <excel.deo99@gmail.com>
2020-02-20 00:00:48 +0700

    nomor 2 done

--- dua_a_b.sh
+++ dua_a_b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # (a) 
-cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 28 > $*.txt
+cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 28 > $*
 
 # (b)
 file="$(echo $* | sed 's/[^A-Za-z]*//g')"
--- soal2.sh
+++ soal2.sh
@@ -1,5 +1,54 @@
-#!/bin/sh
+enkripsi (){
+    str=$1
+    arr=()
+    i=0
+    satu=122
+    jam=`date +%H`
+    while [ "$i" -lt "${#str}" ]; 
+        do
+        kw="$(printf '%d\n' "'${str:$i:1}")"
+        let jumlah=$kw+$jam
+        if [ $jumlah -gt $satu ]
+            then
+            let jumlah-=26
+        fi
+        arr+=("$(printf '%b' $(printf '\\x%x' $jumlah))")
+        i=$((i+1))
+    done
+    apalah="$(printf '%s' "${arr[@]}")"
+    # echo "$apalah"
+
+}
+
+dekripsi (){
+    str=$1
+    arr=()
+    i=0
+    satu=97
+    jam=`date +%H`
+    while [ "$i" -lt "${#str}" ]; 
+        do
+        kw="$(printf '%d\n' "'${str:$i:1}")"
+        let jumlah=$kw-$jam
+        if [ $jumlah -lt $satu ]
+            then
+            let jumlah+=26
+        fi
+        arr+=("$(printf '%b' $(printf '\\x%x' $jumlah))")
+        i=$((i+1))
+    done
+    apalah="$(printf '%s' "${arr[@]}")"
+    # return $apalah
+}
 
 
-echo "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG" | tr '[A-Z]' '[X-ZA-W]'
+# (a) 
+cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 28 > $*.txt
 
+
+# (b)
+file="$(echo $* | sed 's/[^A-Za-z]*//g')"
+namafile="${file%txt}"
+# printf $namafile
+enkripsi $namafile
+mv "$*.txt" "$apalah.txt"
