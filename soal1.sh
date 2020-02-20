da45d8fa59c867e501b17c5067226ea8370a5f0e
exceldeo <excel.deo99@gmail.com>
2020-02-21 01:41:51 +0700

    menambahkan kata" penjelas

--- soal1.sh
+++ soal1.sh
@@ -1,12 +1,14 @@
+echo "soal 1 a"
 # 1 (a)
 awk -F '\t' '{a[$13] += $21} END{for (i in a) print a[i], i }' Sample-Superstore.tsv | sort -g | head -2 | tail -1 
-
+echo "soal 1 b"
 #  1(b)
 awk -F '\t' '/Central/{a[$11] += $21} END{for (i in a) print a[i], i}' Sample-Superstore.tsv | sort -g | head -2
 
+echo "soal 1 c"
 # 1 (c)
 awk -F '\t' '
     {if ( $11=="Illinois" || $11=="Texas" ) 
     a[$17]+=$21} 
     END{ for (i in a) 
-    print a[i], i}' Sample-Superstore.tsv | sort -rg -k 1 | tail -10
+    print a[i], i}' Sample-Superstore.tsv | sort -rg | tail -10
