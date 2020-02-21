0a5d2409d44787ae95111a903a44e85b117dfe10
exceldeo <excel.deo99@gmail.com>
2020-02-21 17:00:09 +0700

    fix no 3a

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -1,1 +1,5 @@
-wget -P /home/excel/Desktop/SoalShiftSISOP20_modul1_E02 -O pdkt_kusuma_ https://loremflickr.com/320/240/cat 
+# 3 (a)
+for ((num=1; num<=28; num=num+1))
+do
+`wget  -c https://loremflickr.com/320/240/cat -O kenangan/pdkt_kusuma_$num -o wget.log` 
+done
