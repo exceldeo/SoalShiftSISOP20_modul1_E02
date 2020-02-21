176ff1b4bba1d856616f65d050e0cf586b5c3def
exceldeo <excel.deo99@gmail.com>
2020-02-22 00:17:09 +0700

    no 3 a fix

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -1,5 +1,33 @@
 # 3 (a)
-for ((num=1; num<=28; num=num+1))
+jumlah=30
+fungsi () {
+    for ((num=1; num<=$jumlah; num=num+1))
+    do
+    # lokasi[$num]=`grep 'Location:' location.log`
+    `wget https://loremflickr.com/320/240/cat -O pdkt_kusuma_$num -a wget.log `
+    # awk -F ' ' ' '
+    done
+}
+fungsi
+
+grep "Location" wget.log > location.log
+readarray line < location.log
+for ((i=0; i<28; i++))
 do
-`wget  -c https://loremflickr.com/320/240/cat -O kenangan/pdkt_kusuma_$num -o wget.log` 
+for ((j=0; j<=i; j++))
+do
+if [ $i == $j ]
+then
+continue
+elif [ "${line[$i]}" == "${line[$j]}" ]
+then
+mv pdkt_kusuma_"$(($i+1))".jpg ./duplicate/duplicate_"$i".jpg
+fi
 done
+done
+
+for ((i=1; i<=28; i++))
+do
+mv pdkt_kusuma_"$i".jpg ./kenangan/kenangan_"$i".jpg
+done
+cp wget.log wget.log.bak
