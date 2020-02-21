88e94a0cdaf425a96428e45bbeff51081abf94b6
qqdnada <nada.qori@gmail.com>
2020-02-21 18:11:54 +0700

    Revisi Soal 1

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,14 +1,25 @@
-echo "soal 1 a"
-# 1 (a)
-awk -F '\t' '{a[$13] += $21} END{for (i in a) print a[i], i }' Sample-Superstore.tsv | sort -g | head -2 | tail -1 
-echo "soal 1 b"
-#  1(b)
-awk -F '\t' '/Central/{a[$11] += $21} END{for (i in a) print a[i], i}' Sample-Superstore.tsv | sort -g | head -2
+#!/bin/bash
 
-echo "soal 1 c"
-# 1 (c)
-awk -F '\t' '
-    {if ( $11=="Illinois" || $11=="Texas" ) 
+# (a)
+region=$(awk -F '\t' '{a[$13] += $21} END{for (i in a) print a[i], i }' Sample-Superstore.tsv | sort -g | awk '{print $2}' | head -2 | tail -1 )
+echo "1.a." $region
+
+i=0
+echo "1.b."
+
+# (b)
+for state in `awk -F '\t' -v region="$region" '($13~region) {a[$11] += $21} END{for (i in a) print a[i], i}' Sample-Superstore.tsv | sort -g | awk '{print $2}' | head -2`
+do
+    arr[$i]=$state
+    echo ${arr[$i]}
+    i=`expr $i + 1`
+done
+
+echo "1.c."
+
+# (c)
+awk -F '\t' -v satu="${arr[0]}" -v dua="${arr[1]}" '
+    {if ( $11==satu || $11==dua)
     a[$17]+=$21} 
     END{ for (i in a) 
-    print a[i], i}' Sample-Superstore.tsv | sort -rg | tail -10
+    print a[i], i}' Sample-Superstore.tsv | sort -rg | awk '{sub($1 FS,""); print}' | tail -10
